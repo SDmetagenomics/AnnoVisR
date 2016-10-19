9c81f397f6553358b2bc00071da72bd5a09b2ecf
Spencer Diamond <Spencer@spenceracintosh.attlocal.net>
2016-10-19 01:23:15 -0700

    Significant Additions to Summary Statistics Mirroring CAZy Analysis

--- Metabolic_HMM_Analysis_Code_v0.5.R
+++ Metabolic_HMM_Analysis_Code_v0.5.R
@@ -1,6 +1,8 @@
 library(plyr)
 library(ggplot2)
 library(pheatmap)
+library(reshape)
+library(vegan)
 
 setwd("~/Desktop/testbed/AnnotateR_Out/Metabolic_HMM_output/")
 
@@ -79,40 +81,44 @@ for (i in 1:length(genome_names)){
   Master_Metabolic_Hits <- merge(Master_Metabolic_Hits, temp_counts, by="Codes", all= TRUE)
 }
 
-rm("temp_counts","temp_table")
+rm("temp_counts","temp_table", "i")
 
 
-### Make NA == 0s and parse out rows of all 0s
+### Make NA == 0s and Parse out Rows of All 0s and Report Genes that had No Hit
 
 Master_Metabolic_Hits[is.na(Master_Metabolic_Hits)] <- 0
+No_Hit <- rowSums(Master_Metabolic_Hits[,5:ncol(Master_Metabolic_Hits)]) == 0
+No_Hit <- Master_Metabolic_Hits[No_Hit,1:4]
+Master_Metabolic_Hits <- Master_Metabolic_Hits[!!rowSums(abs(Master_Metabolic_Hits[-c(1:4)])),]
 
 
-### Load template sheets where numbers of genes within main and sub roles will be calculated
+### Calculate Numbers of Hits within Main and Sub Roles
 
-Main_Categories <- read.delim("~/Desktop/testbed/Metabolic_HMM_Main_Category_Template.txt", header = TRUE)
-Sub_Categories <- read.delim("~/Desktop/testbed/Metabolic_HMM_Sub_Category_Template.txt", header = TRUE)
+Main_Categories <- read.delim("~/Desktop/testbed/AnnoVisR/Annotation_Templates/Metabolic_HMM_Main_Category_Template.txt", header = TRUE)
+Sub_Categories <- read.delim("~/Desktop/testbed/AnnoVisR/Annotation_Templates/Metabolic_HMM_Sub_Category_Template.txt", header = TRUE)
+
 
 Main_Category_Hits <- data.frame()
 
 for (i in 1:nrow(Main_Categories)){
-  Main_Subset <- Metabolic_Hits[grepl(Main_Categories[i,1], Metabolic_Hits$Main_Category),]
+  Main_Subset <- Master_Metabolic_Hits[grepl(Main_Categories[i,1], Master_Metabolic_Hits$Main_Category),]
   temp_row <- colSums(Main_Subset[5:ncol(Main_Subset)])
   Main_Category_Hits <- rbind(Main_Category_Hits, temp_row)
 }
 
 rm("Main_Subset", "temp_row")
 
-###
 
 Sub_Category_Hits <- data.frame()
 
 for (i in 1:nrow(Sub_Categories)){
-  Sub_Subset <- Metabolic_Hits[grepl(Sub_Categories[i,1], Metabolic_Hits$Sub_Category),]
+  Sub_Subset <- Master_Metabolic_Hits[grepl(Sub_Categories[i,1], Master_Metabolic_Hits$Sub_Category),]
   temp_row <- colSums(Sub_Subset[5:ncol(Sub_Subset)])
   Sub_Category_Hits <- rbind(Sub_Category_Hits, temp_row)
 }
 
 rm("Sub_Subset", "temp_row")
+
 
 rownames(Sub_Category_Hits) <- Sub_Categories$Sub_Category
 colnames(Sub_Category_Hits) <- genome_names
@@ -121,7 +127,108 @@ colnames(Sub_Category_Hits) <- genome_names
 rownames(Main_Category_Hits) <- Main_Categories$Main_Category
 colnames(Main_Category_Hits) <- genome_names
 
-pheatmap(Main_Category_Hits, annotation_col = `20cm_Effect`)
+rm("Main_Categories", "Sub_Categories")
+
+
+
+
+#######
+#### Calculate Summary Statistics for Genomes with Respect to Metabolic Enzyme Number and Diversity
+#######
+
+
+Master_Metabolic_Matrix <- Master_Metabolic_Hits[,5:ncol(Master_Metabolic_Hits)]
+
+Metabolic_Variety <- data.frame(Genome_name = Genome_metadata$genome_name, Factor = Genome_metadata$Factor,
+                           Protein_count = Genome_metadata$protein_count,
+                           Unique_Enzymes = NA, All_Enzymes = NA)
+
+
+### Populate Metabolic_Variety DF with Absolute and Unique Domain Hit Numbers 
+
+for (i in 1:ncol(Master_Metabolic_Matrix)){
+  tmp_count <- sum(ifelse(Master_Metabolic_Matrix[,i] > 0, 1, 0))
+  Metabolic_Variety[i,4] <- tmp_count
+  tmp_count <- sum(Master_Metabolic_Matrix[,i])
+  Metabolic_Variety[i,5] <- tmp_count
+}
+
+
+Metabolic_Variety <- data.frame(Metabolic_Variety, 
+                           Unique_Enzymes_protnorm = (Metabolic_Variety$All_Enzymes/Metabolic_Variety$Protein_count)*1000, 
+                           All_Enzymes_protnorm = (Metabolic_Variety$Unique_Enzymes/Metabolic_Variety$Protein_count)*1000)
+
+
+### Produce Diagnostic Scatter Plots Comparing Various Count Statistics
+
+ggplot(Metabolic_Variety, aes(x = Protein_count, y = All_Enzymes, color = Factor)) + 
+  geom_point(size = 3, alpha = 0.8) +
+  stat_smooth(method = "lm") +
+  scale_color_manual(values = c("steelblue", "firebrick3")) +
+  ggtitle("Total CAZy Enzymes vs Genome Protein Number")
+
+
+ggplot(Metabolic_Variety, aes(x = Protein_count, y = Unique_Enzymes, color = Factor)) + 
+  geom_point(size = 3, alpha = 0.8) +
+  stat_smooth(method = "lm") +
+  scale_color_manual(values = c("steelblue", "firebrick3")) +
+  ggtitle("Unique CAZy Enzymes vs Genome Protein Number")
+
+
+ggplot(Metabolic_Variety, aes(x = Unique_Enzymes , y = All_Enzymes, color = Factor)) + 
+  geom_point(size = 3, alpha = 0.8) +
+  stat_smooth(method = "lm") +
+  scale_color_manual(values = c("steelblue", "firebrick3")) +
+  ggtitle("Unique CAZy vs Total CAZy")
+
+
+ggplot(Metabolic_Variety, aes(x = All_Enzymes_protnorm, y = Unique_Enzymes_protnorm, color = Factor)) + 
+  geom_point(size = 3, alpha = 0.8) +
+  stat_smooth(method = "lm") +
+  scale_color_manual(values = c("steelblue", "firebrick3")) +
+  ggtitle("Total vs Unique CAZy Enzymes Prot_Normalized")
+
+
+ggplot(melt(Metabolic_Variety[,2:7]), aes(x = Factor, y = value, fill = Factor)) +
+  geom_boxplot() +
+  facet_wrap(~ variable, scales = "free_y") +
+  ggtitle("Group CAZy Count Statistics") +
+  scale_fill_manual(values = c("steelblue", "firebrick3"))
+
+
+
+
+
+
+############
+
+
+
+
+
+Enzyme_Diversity <- data.frame(Factor = Genome_metadata$Factor, 
+                               Shannon = diversity(t(Master_Metabolic_Matrix)),
+                               Simpson = diversity(t(Master_Metabolic_Matrix), index = "simpson"),
+                               InvSimpson = diversity(t(Master_Metabolic_Matrix), index = "invsimpson"))
+
+
+ggplot(melt(Enzyme_Diversity), aes(x = Factor, y = value, fill = Factor)) +
+  geom_boxplot() + 
+  facet_wrap(~variable, scales = "free_y") +
+  ggtitle("Alpha Diversity Indicies for CAZy Enzymes in Sample Groups") +
+  scale_fill_manual(values = c("steelblue", "firebrick3"))
+
+
+#######
+####  Count Hits to Karthik Metabolic HMMs from Filtered Genomes as well as Main and Sub Role Hits
+#######
+
+
+
+pheatmap(log2(Main_Category_Hits+1), 
+         annotation_col = heat_anno,
+         clustering_method = "ward.D"
+         )
 pheatmap(Sub_Category_Hits)
 
 Log_Main_Category <- Main_Category_Hits + 1
