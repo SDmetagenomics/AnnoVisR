d3865dcc310dff74080819af8af514a9df6e1f71
Spencer Diamond <Spencer@spenceracintosh.attlocal.net>
2016-10-20 11:41:30 -0700

    Updated Annotation template and Significant Updates to Metabolic R code

--- Metabolic_HMM_Analysis_Code_v0.5.R
+++ Metabolic_HMM_Analysis_Code_v0.5.R
@@ -3,6 +3,8 @@ library(ggplot2)
 library(pheatmap)
 library(reshape)
 library(vegan)
+library(corrplot)
+library(Hmisc)
 
 setwd("~/Desktop/testbed/AnnotateR_Out/Metabolic_HMM_output/")
 
@@ -19,6 +21,8 @@ colnames(Genome_metadata) <- c("genome_name", "protein_count", "completeness", "
 heat_anno <- data.frame(Class = Genome_metadata$Factor)
 row.names(heat_anno) <- Genome_metadata$genome_name
 
+
+ann_cols <- list(Class = c(Decrease = "steelblue", Increase = "firebrick3"))
 
 
 
@@ -120,6 +124,8 @@ for (i in 1:nrow(Sub_Categories)){
 rm("Sub_Subset", "temp_row")
 
 
+### Add Role and Genome Lables to Matricies 
+
 rownames(Sub_Category_Hits) <- Sub_Categories$Sub_Category
 colnames(Sub_Category_Hits) <- genome_names
 
@@ -129,6 +135,100 @@ colnames(Main_Category_Hits) <- genome_names
 
 rm("Main_Categories", "Sub_Categories")
 
+### Remove Rows in Main and Sub Category Tables where All Values == 0
+
+Main_Category_Hits <- Main_Category_Hits[!!rowSums(abs(Main_Category_Hits)),]
+Sub_Category_Hits <- Sub_Category_Hits[!!rowSums(abs(Sub_Category_Hits)),]
+
+
+### Construct Binary Presence Absence Tables for Enzyme and Subroles 
+
+
+Master_Metabolic_Matrix <- Master_Metabolic_Hits[,5:ncol(Master_Metabolic_Hits)]
+rownames(Master_Metabolic_Matrix) <- Master_Metabolic_Hits$Gene
+
+Binary_Enzymes_Matrix <- matrix(nrow = nrow(Master_Metabolic_Matrix),
+                                ncol = ncol(Master_Metabolic_Matrix))
+Binary_SubRole_Matrix <- matrix(nrow = nrow(Sub_Category_Hits),
+                                ncol = ncol(Sub_Category_Hits))
+
+
+for (i in 1:ncol(Sub_Category_Hits)){
+  tmp_enz <- ifelse(Master_Metabolic_Matrix[,i] > 0, 1, 0)
+  tmp_sub <- ifelse(Sub_Category_Hits[,i] > 0, 1, 0)
+  Binary_Enzymes_Matrix[,i] <- tmp_enz
+  Binary_SubRole_Matrix[,i] <- tmp_sub
+}
+
+rm("tmp_enz", "tmp_sub", "i")
+
+rownames(Binary_SubRole_Matrix) <- rownames(Sub_Category_Hits)
+colnames(Binary_SubRole_Matrix) <- colnames(Sub_Category_Hits)
+
+rownames(Binary_Enzymes_Matrix) <- rownames(Master_Metabolic_Matrix)
+colnames(Binary_Enzymes_Matrix) <- colnames(Master_Metabolic_Matrix)
+
+rowSums(Sub_Category_Hits)
+
+
+
+library(ropls)
+pls_DA_model <- opls(t(Binary_SubRole_Matrix), Genome_metadata$Factor, crossvalI = 10, permI = 1000)
+
+Model_QRParams <- pls_DA_model@modelDF
+Model_QRParams <- data.frame(Component = c(1,2,1,2),
+                             melt(Model_QRParams, measure.vars = c("R2X(cum)", "Q2(cum)")))
+
+
+ggplot(Model_QRParams, aes(x = Component, y = value, fill = variable)) + 
+  geom_bar(stat = "identity", position = "dodge", color = "black") +
+  scale_fill_manual(values = c("steelblue", "firebrick3")) +
+  ggtitle("Cumulative Metrics as Components Added")
+
+
+
+Scores <- data.frame(class = Genome_metadata$Factor, pls_DA_model@scoreMN)
+
+ggplot(Scores, aes(x = p1, y = p2, color = class, fill = class)) + geom_point(size = 3) + 
+  stat_ellipse(geom="polygon", alpha = 0.5) + scale_fill_manual(values = c("steelblue", "firebrick3")) +
+  scale_color_manual(values = c("steelblue", "firebrick3"))
+
+Loadings <- data.frame(Variable = rownames(pls_DA_model@loadingMN), pls_DA_model@loadingMN[,1:2])
+
+ggplot(Loadings, aes(x = p1, y = p2, label = Variable)) + geom_point(alpha = 0.5, size = 3, color = "purple") +
+  geom_text(nudge_x = 0.08) + geom_hline(yintercept = 0) + geom_vline(xintercept = 0) +
+  expand_limits(x = c(-.5,.5), y = c(-.5,.5)) + ggtitle("Loadings")
+
+ggplot() + geom_point(data = Scores, aes(x = p1, y = p2, color = class, fill = class), size = 3) + 
+  #stat_ellipse(data = Scores, aes(x = p1, y = p2, color = class, fill = class), geom="polygon", alpha = 0.5) + 
+  geom_segment(data = Loadings, aes(x = 0, y = 0, xend = 10*p1, yend = 10*p2), linetype = 5) +
+  geom_text(data = Loadings, aes(x = 10*p1, y = 10*p2, label = Variable), nudge_x = -.5, size = 2) +
+  scale_color_manual(values = c("steelblue", "firebrick3"))
+
+
+
+
+
+
+res <- rcorr(t(Binary_SubRole_Matrix),type = "spearman")
+
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
+
+flat_corr <- flattenCorrMatrix(res$r, res$P)
+
+corrplot(res$r, type="upper", order="FPC", 
+         p.mat = res$P, sig.level = 0.01, 
+         insig = "blank", tl.col = "black")
+
+
 
 
 
@@ -136,8 +236,6 @@ rm("Main_Categories", "Sub_Categories")
 #### Calculate Summary Statistics for Genomes with Respect to Metabolic Enzyme Number and Diversity
 #######
 
-
-Master_Metabolic_Matrix <- Master_Metabolic_Hits[,5:ncol(Master_Metabolic_Hits)]
 
 Metabolic_Variety <- data.frame(Genome_name = Genome_metadata$genome_name, Factor = Genome_metadata$Factor,
                            Protein_count = Genome_metadata$protein_count,
@@ -155,8 +253,8 @@ for (i in 1:ncol(Master_Metabolic_Matrix)){
 
 
 Metabolic_Variety <- data.frame(Metabolic_Variety, 
-                           Unique_Enzymes_protnorm = (Metabolic_Variety$All_Enzymes/Metabolic_Variety$Protein_count)*1000, 
-                           All_Enzymes_protnorm = (Metabolic_Variety$Unique_Enzymes/Metabolic_Variety$Protein_count)*1000)
+                           Unique_Enzymes_protnorm = (Metabolic_Variety$Unique_Enzymes/Metabolic_Variety$Protein_count)*1000, 
+                           All_Enzymes_protnorm = (Metabolic_Variety$All_Enzymes/Metabolic_Variety$Protein_count)*1000)
 
 
 ### Produce Diagnostic Scatter Plots Comparing Various Count Statistics
@@ -241,3 +339,12 @@ Log_Sub_Category <- apply(Log_Sub_Category, 2 , log2)
 pheatmap(Log_Sub_Category, clustering_method = "ward.D2", annotation_col = `20cm_Effect`)
 
 pheatmap(t(ordered_matrix), cluster_cols = FALSE, cluster_rows = FALSE, annotation_col = `20cm_Effect`)
+
+
+
+#######
+####  Perform Enrichment Statistics 
+#######
+
+
+
